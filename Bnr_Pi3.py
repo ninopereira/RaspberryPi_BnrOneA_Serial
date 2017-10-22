596962640fadb2814b6a80344660e04d46ebf9c0
Nino Pereira <ninopereira.pt@gmail.com>
2017-10-22 16:43:43 +0100

    feat: add separate file for python Botnroll control.

---
+++ Bnr_Pi3.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# code for python 3
+import time
+import serial
+import io
+
+ser = serial.Serial(
+    port='/dev/ttyS0',
+    baudrate = 19200,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=0.1
+)
+sio = io.TextIOWrapper(io.BufferedRWPair(ser, ser))
+print ('Serial Connection Ready!')
+
+count = 0
+while True:
+    data_in=sio.readline()
+    if data_in:
+        print (str(data_in))
+        data_out = "CMD_VEL " + str(-count)  + " " + str(count) + "\n"
+        sio.write(data_out)
+        sio.flush()
+        count += 10
+        count = count % 100
